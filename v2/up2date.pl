7a9d4710e98615e4a7203b38ab1e9112652280b3
Frank Breedijk <fbreedijk@schubergphilis.com>
2014-05-12 15:06:15 +0200

    Crude way to kill and XSS in an outdted API

--- v2/up2date.pl
+++ v2/up2date.pl
@@ -10,63 +10,16 @@
 use strict;
 use CGI;
 
-my $current = "2.0 or higher";
-my $beta = "xxxxx";
-my $cool = "xxxxx";
-my ( $one, $two, $three) = split /\./, $current;
-
-
-#my (
-   #);
-
 my $query = CGI::new();
 
 print $query->header("text/xml");
 
-my $version = $query->param("version") or result(1,"No version specified", "unknown");
-my $reported_version = $version;
-$version =~ s/\.B\d+//;
-
-
-my ($major, $minor, $revision) = split /\./, $version;
-
-if ($major < $one ) {
-	result(1,"This check only supports version 2.x.x",$version);
-} elsif ( $version eq $current ) {
-	result(0,"Your version $reported_version is up to date",$version);
-} elsif ( $version eq $cool ) {
-	result(0,"",$version);
-} elsif ( $minor lt $two || ($minor eq $two && $revision lt $three) ) {
-	result(1,"<b>Version $current is available, please upgrade...</b><br>
-		<a href=https://github.com/schubergphilis/Seccubus_v2/downloads>
-		Download it from GitHub</a>
-",$version);
-} elsif ( $version eq $beta ) {
-	result(0,"Your version ($reported_version) is the trunk version ($version) of Seccubus",$version);
-} else {
-	result(1,"Unrecognized version: $version, current is $current",$version);
-}
-
-exit;
-
-sub result($$$;) {
-	my $iserror = shift;
-	my $message = shift;
-	my $version = shift;
-
-	my $status = "OK";
-	if ( $iserror ) { 
-		$status = "NOK";
-	}
-		
-
-	print "
+print "
 <seccubusAPI name='up2date.pl'>
-	<result>$status</result>
-	<message>$message</message>
+	<result>1</result>
+	<message>This version of Seccubus has been abandoned. Donwload the latest version please</message>
 	<data>
-		<version>$version</version>
+		<version>Outdated</version>
 	</data>
 </seccubusAPI>";
-	exit;
-}
+exit;
