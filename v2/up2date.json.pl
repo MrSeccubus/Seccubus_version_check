048353cdc9a6eba308dd5ae1a91da0c098770a69
blabla1337 <glenntencate@gmail.com>
2014-08-01 13:55:10 +0200

    Update up2date.json.pl

--- v2/up2date.json.pl
+++ v2/up2date.json.pl
@@ -46,37 +46,36 @@ Release notes:
 
 Seccubus V2 Read Me
 ===================
-Seccubus automates regular vulnerability scans with various tools and aids
-security people in the fast analysis of its output, both on the first scan and
+Seccubus automates regular vulnerability scans with vrious tools and aids 
+security people in the fast analysis of its output, both on the first scan and 
 on repeated scans.
 
-On repeated scan delta reporting ensures that findings only need to be judged
+On repeated scan delta reporting ensures that findings only need to be judged 
 when they first appear in the scan results or when their output changes.
 
-Seccubus 2.x is the only actively developed and maintained branch and all support
-for Seccubus V1 has officially been dropped.
+Seccubus 2.x is the only actively developed and maintained branch and all support 
+for Seccubus V1 has officially been dropped. 
 
 Seccubus V2 works with the following scanners:
 * Nessus 4.x and 5.x (professional and home feed)
+* Skipfish
 * OpenVAS
+* Medusa (local and remote)
 * Nikto (local and remote)
 * NMap (local and remote)
-* Skipfish (local and remote)
 * SSLyze
 
 For more information visit [www.seccubus.com]
 
+01-08-2014 - 2.8 - New scanner and Burp parser
+============================================
+Medusa is added to scanner tools thnx to Arkanoi
+Added burp parser to ivil thnx to SphaZ 
+
 21-05-2014 - 2.6 - Bug Fixes
 ============================================
-
-Key new features / issues resolved
-----------------------------------
 A couple of bugs are now fixed thanks to Arkanoi and SphaZ
 
-Bugs fixed (tickets closed):
-----------------------------
-Issue #105 - please mask scanner passwords 
-Issue #106 - long Nessus scan results are not loaded
 
 },
 "https://www.seccubus.com/seccubus-v2-8/",""];
