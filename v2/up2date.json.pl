8eb03abdf9d673d6dae5dfa67c9d610b5a10406d
Frank Breedijk <fbreedijk@schubergphilis.com>
2016-07-12 18:18:07 +0200

    Released 2.26

--- v2/up2date.json.pl
+++ v2/up2date.json.pl
@@ -11,9 +11,9 @@ use strict;
 use CGI;
 use JSON;
 
-my $current = "2.24";
-my $beta = "2.25";
-my $cool = "2.26";
+my $current = "2.26";
+my $beta = "2.27";
+my $cool = "2.28";
 my ( $one, $two, $three) = split /\./, $current;
 $three = 'ZZZ' unless $three;
 my $data = [];
@@ -45,23 +45,43 @@ if ( ! $version ) {
 
 Release notes
 
-22-04-2016 - 2.24 - RPM and cert improvements
-=============================================
+12-7-2016 - 2.26 - Speed improvements and a whole log of bugfixes
+=================================================================
+This release is especially interesting for those of you that are working with large result sets.
+The number of findigns that is returned is now limited to 200 results by default and can be adjusted
+up or down.
+A lot of the filter logic has been moved from the Perl backend to more intelligent database queries 
+where caching and other optimalisations techniques prevent timeouts when working with larger result
+sets.
 
-Not everything went perfect upgrading the build process, so we had to tweak the RPM a little and fix an error in the UpToDate.pl script.
+Additional improvements are done the rpm packaging and the Nessus6 scanner wich now no longer depends
+on certain excotic perl modules.
+
+The number of change records that is created and displayed has been reduced and some other more minor 
+have been fixed too.
 
 Enhancements
 ------------
-* #310 - Root CA for v2.seccubus.com ([LetsEncrypt](https://letsencrypt.org/)) is now pinned for the version check
-* #316 - Clarify create database and grant statement in ConfigTest.pl
+* #128 - Limit the amount of findings that is returned from the back end 
+* #312 - SSLLabs and Nessus6 scanner no longer depend on perl-REST-Client
+* #319 - RPM now builds and installs under CentOs/RHEL 5 too
+* #320 - Nessus6 scanner now downloads PDF and HTML version of report too
+* #322 - Removed old scanners: Nessus v5 and earlier, OpenVAS v5 and earlier
+* Improved exit codes for the onlyonxday.sh utility
 
 Bug Fixes
 ---------
-* #310 - Version check does not like my certificate
-* #311 - RPM: Config could not be found after version upgrade to 2.22
-* #313 - RPM: Seccubus.conf not placed in correct directory (v2.22)
-* #314 - RPM: v2.22 /opt/seccubus/www/dev should not exist
-* #315 - RPM: v2.22 dependancy mysql-server is not installed 
+* #344 - Nessus6 scanner script using LWP::UserAgent is too brittle
+* #330 - Add perl-LWP-Protocol-https as RPM dependency
+* #328 - CopyRight year unit test appears to be broken on Travis
+* #327 - OpenVAS target is always created with the default portlist
+* #323 - Non-critical warnings in unit tests fixed
+* #333 - LWP::UserAgent is missing method delete on RH5 and RH6
+* #305 - Finding change records are generated even if a finding did not actually change
+* #300 - Editing an issue and updating the severity doesn't work
+* #295 - Trigger in notification edit will fall back to previous on re-edit
+* #277 - Remove redundant documentation from source tree
+* #183 - SSL validation ingore not corretly implemented
 },
 "https://github.com/schubergphilis/Seccubus_v2/releases",""];
 	} elsif ( $version eq $beta ) {
