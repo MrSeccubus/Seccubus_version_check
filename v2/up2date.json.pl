bff57ec597016227ad8a84198cb49baecc333132
Frank Breedijk <fbreedijk@schubergphilis.com>
2017-04-13 14:07:43 +0200

    Version v2.30 is out

--- v2/up2date.json.pl
+++ v2/up2date.json.pl
@@ -11,9 +11,9 @@ use strict;
 use CGI;
 use JSON;
 
-my $current = "2.28";
-my $beta = "2.29";
-my $cool = "2.30";
+my $current = "2.30";
+my $beta = "2.31";
+my $cool = "2.32";
 my ( $one, $two, $three) = split /\./, $current;
 $three = 'ZZZ' unless $three;
 my $data = [];
@@ -45,39 +45,36 @@ if ( ! $version ) {
 
 Release notes
 
-For my work at Schuberg Philis we wanted to run Seccubus in Docker 
-containers. This and inspiration from Karl Newell (see https://hub.docker.com/r/karlnewell/seccubus/) 
-caused me to add a Dockerfile (and some other files) to Seccubus so that Seccubus can now be 
-run in a docker container. 
+One of the things that has been lacking for a while were proper workspace export and import tools. This release adds them and fixes another issue that has been on our whishlist for long, better handling of gone findings reappearing.
 
-In addition I fixed a couple of bugs and changed the ssllabs scanner so it now uses the v3 API endpoint.
+Now when a finding that was previously marked as gone reappears in a scan the 'status before gone' is taken into account.
+
+When the 'status before gone' was:
+* New - The finding will reappear as new
+* Changed - The finding will reappear as changed
+* Open - The finding will reappear as open, unless the finding text has changed, then it will reappear as changed
+* No issue - The finding wil reappear as no issue, unless the finding text has changed, then it will reappear as changed
+* MASKED - The finding will stay MASKED
 
 Enhancements
 ------------
-* #361 - arkenoi created a netsparker2ivil tool that allows you to manually import Netsparker scans
-* #331 - Now using SSLLabs API v3
-* #386 - New SSL labs API output featues incorporporated
-* #389 - API endpoint URL has moved to a single function so it can be patched if deployed in a 
-         three tier architecture
-* #392 - Alternative handling of the updateFIndings.pl API
-* #397 - Allow seccubus to authenticate via an http request header
-* #399 - Create a Docker container for Seccubus
+* #126 - Delta engine improved: Beter recovery from GONE findings
+* #257 - Import/export tools added
+* #408 - Seccubus now refuses to load an ivil file with 0 findings
+* #412 - Disabled tofu to enhance Docker support
+* #419 - Enable crontab support in docker images
+* #423 - Show futureGrade in SSLlabs scan results
 
 Bug Fixes
 ---------
-* #364 - auto_gen column was missing from asset_host table
-* #358 - Could not get findings when an asset was used for the query
-* #360 - Not able to export report in PDF format - This breaks the scan
-* #336 - Non-critical RPM errors on CentOS 5
-* #376 - Removed 50 host limit in filters as it was counterproductive
-* #374 - Fixed Nikto path detection code
-* #377 - Hostname filter wasn't working correctly, typed Hostname iso HostName
-* #385 - SSLlabs failed because cypher preference order was split out per protocol by SSLlabs now.
-* #394 - SSLlabs scanner failed if all enpoints fail and --gradeonly was used
+* #403 - SSLlabs scanner help file was not up to date
+* #414 - Mkdir error will no longer appear if entrypoint.sh is run twice
+* #426 - Corrections to README.md from Karol Kozakowski merged
+* #432 - StaticPkpPolicy not recognized
 },
 "https://github.com/schubergphilis/Seccubus_v2/releases",""];
 	} elsif ( $version eq $beta ) {
-		$data = ["OK", "Your version ($r_version) is the trunk version ($version) of Seccubus, proceed at your own risk",""];
+		$data = ["OK", "Your version ($r_version) is the active development version ($version) of Seccubus, it includes the latest features but may include the latest artifacts as well ;)",""];
 	} else {
 		$data = ["Error","Unrecognized version: $version, current is $current","http://seccubus.com"];
 	}
